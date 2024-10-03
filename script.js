a3d5dfe61e7f34c52c4906a98ef74fa709f64017
William Lint <wilrlin2@email.ecok.edu>
2024-10-02 21:27:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function initMap(){
-  map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getELementById('map'), {
     center: {lat: 34.644, lng: -97.161},
     zoom: 15,
     mapId:'884847b6d71f2d9c'
