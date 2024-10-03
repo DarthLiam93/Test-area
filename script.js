f35ba7df04e19b2456cb5a9ff12513766f820719
William Lint <wilrlin2@email.ecok.edu>
2024-10-02 21:17:09 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function initMap(){
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 34.644, lng: -97.161},
+    zoom: 15,
+    mapId:'884847b6d71f2d9c'
+});
+}
+                              
