d3a024226eecb919f804bc046ac455ab621da618
William Lint <wilrlin2@email.ecok.edu>
2024-10-09 12:02:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,24 +8,4 @@ async function initMap() {
     zoom: 15,
   });
 }
-
-  const marker = new google.maps.marker.AdvancedMarkerElement({
-    position: myLatlng,
-    map,
-    title: "Click to zoom",
-  });
-
-  map.addListener("center_changed", () => {
-    // 3 seconds after the center of the map has changed, pan back to the
-    // marker.
-    window.setTimeout(() => {
-      map.panTo(marker.position);
-    }, 3000);
-  });
-  marker.addListener("click", () => {
-    map.setZoom(8);
-    map.setCenter(marker.position);
-  });
-}
-
 initMap();
