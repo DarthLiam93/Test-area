d70860f3228bdd6cd5adb6d8f893ed4077b53653
William Lint <wilrlin2@email.ecok.edu>
2024-10-14 11:10:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,23 @@ let map;
 
 async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
-  var kmlLayer = new google.maps.KmlLayer();
-  
+
+  // Initialize the map
   map = new Map(document.getElementById("map"), {
-    center: { lat: 34.644, lng: -97.161} ,
+    center: { lat: 34.644, lng: -97.161 },
     zoom: 15,
-    var src = 'https://console.cloud.google.com/google/maps-apis/datasets?project=wynnewood-primary-lines';
-    var kmlLayer = new google.maps.KmlLayer(src, {
-  suppressInfoWindows: true,
-  preserveViewport: false,
-  map: map
-});
+  });
+
+  // Set the source for the KML layer
+  const src = 'https://console.cloud.google.com/google/maps-apis/datasets?project=wynnewood-primary-lines';
+  
+  // Create the KML layer
+  const kmlLayer = new google.maps.KmlLayer({
+    url: src,
+    suppressInfoWindows: true,
+    preserveViewport: false,
+    map: map
   });
 }
+
 initMap();
