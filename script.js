8b6f6e70949b97fdb238b760e2971af15da9fc63
William Lint <wilrlin2@email.ecok.edu>
2024-10-14 22:56:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let map;
 
 async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
-  const src = 'https://www.google.com/maps/d/u/0/kml?forcekml=1&mid=1dA7PUZTCP12TZ5QE0QaaSbspttkbpZs&lid=0521LZf3y08';
 
   // Initialize the map
   map = new Map(document.getElementById("map"), {
@@ -10,8 +9,17 @@ async function initMap() {
     zoom: 15,
   });
 
-const ctaLayer = new google.maps.KmlLayer({
-    url: "https://googlearchive.github.io/js-v2-samples/ggeoxml/cta.kml",
-    map: map,
+  // Set the source for the KML layer
+  const src = 'https://github.com/DarthLiam93/Test-area/blob/test-branch/WynnewoodPrimaryLines.kml';
+  const src = 'https://www.google.com/maps/d/u/0/kml?forcekml=1&mid=1dA7PUZTCP12TZ5QE0QaaSbspttkbpZs&lid=0521LZf3y08';
+
+  // Create the KML layer
+  const kmlLayer = new google.maps.KmlLayer({
+    url: src,
+    suppressInfoWindows: true,
+    preserveViewport: false,
+    map: map
   });
 }
+
+initMap();
