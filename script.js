72f29761d6f7a03bdf52826f751c5b96ea16a3ef
William Lint <wilrlin2@email.ecok.edu>
2024-10-14 22:58:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ async function initMap() {
 
   // Set the source for the KML layer
   const src = 'https://github.com/DarthLiam93/Test-area/blob/test-branch/WynnewoodPrimaryLines.kml';
-  const src = 'https://www.google.com/maps/d/u/0/kml?forcekml=1&mid=1dA7PUZTCP12TZ5QE0QaaSbspttkbpZs&lid=0521LZf3y08';
+  const src = 'https://https://github.com/DarthLiam93/Test-area/blob/test-branch/WynnewoodPrimaryLines.kml';
 
   // Create the KML layer
   const kmlLayer = new google.maps.KmlLayer({
