57c353c55bea7ae5cc65679c2ce98853ab2679ef
William Lint <wilrlin2@email.ecok.edu>
2024-10-14 22:54:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,17 +7,12 @@ async function initMap() {
   map = new Map(document.getElementById("map"), {
     center: { lat: 34.644, lng: -97.161 },
     zoom: 15,
+  });
 
-
-  // Create the KML layer
+  // Set the source for the KML layer
+  const src = 'https://www.google.com/maps/d/u/0/kml?forcekml=1&mid=1dA7PUZTCP12TZ5QE0QaaSbspttkbpZs&lid=0521LZf3y08';
 const ctaLayer = new google.maps.KmlLayer({
-    url: "https://www.google.com/maps/d/u/0/kml?forcekml=1&mid=1dA7PUZTCP12TZ5QE0QaaSbspttkbpZs&lid=0521LZf3y08",
+    url: "https://googlearchive.github.io/js-v2-samples/ggeoxml/cta.kml",
     map: map,
   });
-    suppressInfoWindows: false,
-    preserveViewport: false,
-    map: map
-  }); });
 }
-
-initMap();
