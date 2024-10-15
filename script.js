d1b4736a37b60227f0d4690860db410fc3bf0e5c
William Lint <wilrlin2@email.ecok.edu>
2024-10-14 22:55:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let map;
 
 async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
+  const src = 'https://www.google.com/maps/d/u/0/kml?forcekml=1&mid=1dA7PUZTCP12TZ5QE0QaaSbspttkbpZs&lid=0521LZf3y08';
 
   // Initialize the map
   map = new Map(document.getElementById("map"), {
@@ -9,8 +10,6 @@ async function initMap() {
     zoom: 15,
   });
 
-  // Set the source for the KML layer
-  const src = 'https://www.google.com/maps/d/u/0/kml?forcekml=1&mid=1dA7PUZTCP12TZ5QE0QaaSbspttkbpZs&lid=0521LZf3y08';
 const ctaLayer = new google.maps.KmlLayer({
     url: "https://googlearchive.github.io/js-v2-samples/ggeoxml/cta.kml",
     map: map,
