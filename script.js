221fd388d5e7f25609aee1ed195da462d8f5048e
William Lint <wilrlin2@email.ecok.edu>
2024-10-14 22:49:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ async function initMap() {
   });
 
   // Set the source for the KML layer
-  const src = 'https://github.com/DarthLiam93/Test-area/blob/test-branch/WynnewoodPrimaryLines.kml';
   const src = 'https://www.google.com/maps/d/u/0/kml?forcekml=1&mid=1dA7PUZTCP12TZ5QE0QaaSbspttkbpZs&lid=0521LZf3y08';
 
   // Create the KML layer
